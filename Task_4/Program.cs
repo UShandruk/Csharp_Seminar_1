191b720be34e8376b3417b08318e5cfd507230e9
Ulyana Shandruk <ulyana.shandruk@gmail.com>
2023-10-10 17:49:42 +0300

    Задача 4 (условие)

---
+++ Task_4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задание 4
+// Внутри класса Answer напишите метод PrintEvenNumbers,
+// которая на вход принимает число (number),
+// а на выходе выводит все чётные числа от 1 до number (включительно), разделеные знаком табуляции.
+// https://autotest.gb.ru/problems/5
+
+using System;
+
+public class Answer {
+    static string PrintEvenNumbers(int number)
+    {
+      
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 6;
+        }
+
+        // Не удаляйте строки ниже
+        Console.Write(PrintEvenNumbers(number));
+    }
+}
